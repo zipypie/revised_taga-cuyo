5ab172de12afc8224b7eb8e87f627443651b9b0e
zipypie <201980170@psu.palawan.edu.ph>
2024-12-15 01:29:38 +0800

    remove logger

--- lib/core/config/translate_api.dart
+++ lib/core/config/translate_api.dart
@@ -1,7 +1,6 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:taga_cuyo/core/models/translate_model.dart';
-import 'package:taga_cuyo/core/utils/logger.dart';
 
 class TranslateApi {
   final String apiUrl =
@@ -14,10 +13,6 @@ class TranslateApi {
         headers: {"Content-Type": "application/json"},
         body: jsonEncode(request.toJson()), // Use the model's toJson method
       );
-
-      // Log the response to debug
-      Logger.log('Response status: ${response.statusCode}');
-      Logger.log('Response body: ${response.body}');
 
       if (response.statusCode == 200) {
         final data = jsonDecode(response.body);
